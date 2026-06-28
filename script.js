2ebfe3d9b354ff67a134a7d3867aea3e5b520f6f
Julian Leal <jleal7@twu.edu>
2026-06-27 22:03:25 -0500

    changed layout for index.html

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".clickme").addEventListener("click", () => {
-    document.querySelectorAll(".hidden").forEach((item) => {
-        item.classList.toggle("showing");
-    });
-});
